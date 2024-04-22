9c11df345de2da19853849dbb3e7bf2eb2dc223e
Ahmed Essam <120462370+sanadorphans@users.noreply.github.com>
2024-04-22 13:01:30 +0200

    commit

--- resources/views/cms/team_members/index.blade.php
+++ resources/views/cms/team_members/index.blade.php
@@ -2,6 +2,12 @@
 
 @section('page_name') {{ __('lang.staff') }} @endsection
 
+@php
+    $name = 'name' . '_' . app()->getLocale();
+    $details = 'details' . '_' . app()->getLocale();
+    $position = 'position' . '_' . app()->getLocale();
+@endphp
+
 
 @section('style')
     <link rel="stylesheet" href="{{asset('css/Staff.css?v=1.6')}}">
@@ -18,7 +24,13 @@
         </div>
         <div class="staff">
             @forelse($team_members as $team_member)
-                @include('cms.team_members.components.team_member')
+                <div class="member">
+                    <a class="staffImage" href="{{ route('web.team_members.show',$team_member->id) }}" aria-label="{{ $team_member->$name }}"><div style="--background: url(../storage/{{str_replace("\\" , "/",$team_member->image)}})"></div></a>
+                    <div class="info">
+                        <h1> {{ $team_member->$name ? $team_member->$name : $team_member->name }}</h1>
+                        <p> {{ $team_member->$position ? $team_member->$position : $team_member->name }}</p>
+                    </div>
+                </div>
             @empty
                 <div>{{ __('lang.no_data') }}</div>
             @endforelse

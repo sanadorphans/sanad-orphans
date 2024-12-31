8d321fe511b6044afb2b4c8852762ad0401db092
Sanad Orphans <120462370+sanadorphans@users.noreply.github.com>
2024-12-31 12:52:56 +0200

    Update DonationsController.php

--- app/Http/Controllers/DonationsController.php
+++ app/Http/Controllers/DonationsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DonationController extends Controller
+class DonationsController extends Controller
 {
     
     public function index()
